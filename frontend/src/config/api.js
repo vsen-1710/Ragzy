e4aac1088e70fa7b87db42a66342a164865f7817
Vishal Raj <vishal@cloudastra.in>
2025-05-30 18:35:58 +0530

    fixing issues

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: process.env.REACT_APP_API_URL ||  'https://ragzy.onrender.com'
+  BASE_URL: process.env.REACT_APP_API_URL || 'https://8078-2405-201-4019-6aa0-c9bf-5e1f-7b02-4e2f.ngrok-free.app'
  // 10 seconds
 };
  
